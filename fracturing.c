10581d87d25e0032b7abbec490746f1e4ae11a0a
Sugistic <ethankayuor@gmail.com>
2024-09-09 02:57:42 +0000

    created all functions, trying to get the main functiong to call all of them

--- fracturing.c
+++ fracturing.c
@@ -1,45 +1,70 @@
+//stdlib.h is for the absoluate value helper function 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-
-double calculateDistance();
+int main(void){
     //setting my variables to use later
-        double dx1;
-        double dx2;
-        double dy1;
-        double dy2;
-        double distance;
-        //getting user input for variable coordinate points
-        scanf("%lf",&dx1);
-        scanf("\n%lf",&dx2);
-        scanf("\n%lf",&dy1);
-        scanf("\n%lf",&dy2);
+    double x1;
+    double x2;
+    double y1;
+    double y2;
+    //getting user input for variable coordinate points
+    scanf("%lf",&x1);
+    scanf("\n%lf",&x2);
+    scanf("\n%lf",&y1);
+    scanf("\n%lf",&y2);
+
+
+    double calculateDistance();
         //calculating distance then printing the user's coordinate points and the distance between them
-        distance = ((dx2-dx1)*(dx2-dx1))+((dy2-dy1)*(dy2-dy1));
-        printf("\nPoint #1 entered: x1 = %lf; y1 = %lf",dx1,dy1);
-        printf("\nPoint #2 entered: x2 = %lf; y2 = %lf",dx2,dy2);
-        printf("\nThe distance between the two points is %lf",distance);
+        double distance;
+        distance = ((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1));
+        printf("\nPoint #1 entered: x1 = %lf; y1 = %lf",x1,y1);
+        printf("\nPoint #2 entered: x2 = %lf; y2 = %lf",x2,y2);
+        printf("\nThe distance between the two points is %lf\n",distance);
         return distance;
 
+    double calculateWidth();
+        double width;
+        width = (abs(x2-x1));
+        printf("\nPoint #1 entered: x1 = %lf; y1 = %lf",x1,y1);
+        printf("\nPoint #2 entered: x2 = %lf; y2 = %lf",x2,y2);
+        printf("The width of the city encompassed by your request is %lf",width);
+
+    double calculateHeight();
+        double height;
+        width = (abs(y2-y1));
+        printf("\nPoint #1 entered: x1 = %lf; y1 = %lf",x1,y1);
+        printf("\nPoint #2 entered: x2 = %lf; y2 = %lf",x2,y2);
+        printf("The height of the city encompassed by your request is %lf",height);
+
     double calculatePerimeter();
-    //setting up the perimeter coorindate values
-        double px1;
-        double px2;
-        double py1;
-        double py2;
+    //calling the width and height functions in order to calculate perimeter
+        calculateHeight();
+        calculateWidth();
         double perimeter;
-        //getting user input for coordinate points for perimter
-        scanf("\n%lf",&px1);
-        scanf("\n%lf",&px2);
-        scanf("\n%lf",&py1);
-        scanf("\n%lf",&py2);
-        //calculating distance then printing the user's coordinate points and the distance between them
-        distance = ((dx2-dx1)*(dx2-dx1))+((dy2-dy1)*(dy2-dy1));
-        printf("\nPoint #1 entered: x1 = %lf; y1 = %lf",px1,py1);
-        printf("\nPoint #2 entered: x2 = %lf; y2 = %lf",px2,py2);
-        printf("\nThe distance between the two points is %lf",distance);
+        perimeter = ((width*2) + (perimeter*2));
+        printf("\nPoint #1 entered: x1 = %lf; y1 = %lf",x1,y1);
+        printf("\nPoint #2 entered: x2 = %lf; y2 = %lf",x2,y2);
+        printf("\nThe perimeter between the two points is %lf",perimeter);
         int opinion = 5;
         return opinion;
+    double calculateArea();
+        double area;
+        calculateHeight();
+        calculateWidth();
+        area = (width * perimeter);
+        printf("\nPoint #1 entered: x1 = %lf; y1 = %lf",x1,y1);
+        printf("\nPoint #2 entered: x2 = %lf; y2 = %lf",x2,y2);
+        printf("\nThe area between the two points is %lf",area);
+    
+
+
+
+
+
+
+    return 0;
 
 
 
